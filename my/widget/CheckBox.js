349f52cfae5624a48aa19e0ce983b94e205bcf24
zhangjin <zhangjin@kingyea.com.cn>
2016-05-14 14:14:11 +0800

    判断资源是否加载

--- my/widget/CheckBox.js
+++ my/widget/CheckBox.js
@@ -1,34 +1,39 @@
-dojo.provide("my.widget.CheckBox");
+if(!dojo._hasResource["my.widget.CheckBox"]){
+	dojo._hasResource["my.widget.CheckBox"]=true;
+	
+	dojo.provide("my.widget.CheckBox");
 
-dojo.require("dijit.form.CheckBox");
-dojo.declare("my.widget.CheckBox",dijit.form.CheckBox, {
-	
-	startup : function() {
-		this.inherited(arguments);
-		// 使用jQuery
-		//var html = '<label for="' +this.id+ '" style="display:inline;float: none;padding-right:0;">' +this.labelText+ '</label>';
-		//$(this.domNode).after(html);
+	dojo.require("dijit.form.CheckBox");
+	dojo.declare("my.widget.CheckBox",dijit.form.CheckBox, {
 		
-		// 直接使用JS
-		var label = document.createElement("label");
-		label.setAttribute("for", this.id);
-		label.innerText = this.labelText;
-		label.style.display = "inline";
-		label.style.float = "none";
-		label.style.paddingRight = "0";
+		startup : function() {
+			this.inherited(arguments);
+			// 使用jQuery
+			//var html = '<label for="' +this.id+ '" style="display:inline;float: none;padding-right:0;">' +this.labelText+ '</label>';
+			//$(this.domNode).after(html);
+			
+			// 直接使用JS
+			var label = document.createElement("label");
+			label.setAttribute("for", this.id);
+			label.innerText = this.labelText;
+			label.style.display = "inline";
+			label.style.float = "none";
+			label.style.paddingRight = "0";
+			
+			this.insertAfter(label, this.domNode);
+		},
 		
-		this.insertAfter(label, this.domNode);
-	},
-	
-	insertAfter : function (newElement, targetElement) {
-   		var parent = targetElement.parentNode;
-   		if (parent.lastChild == targetElement ) {
-        	// 如果最后的节点是目标元素，则直接添加。因为默认是最后
-        	parent.appendChild(newElement );
-   		} else {
-        	//如果不是，则插入在目标元素的下一个兄弟节点的前面。也就是目标元素的后面
-        	parent.insertBefore( newElement, targetElement.nextSibling );
-   		}
-	},
-	labelText : ""
-});
+		insertAfter : function (newElement, targetElement) {
+	   		var parent = targetElement.parentNode;
+	   		if (parent.lastChild == targetElement ) {
+	        	// 如果最后的节点是目标元素，则直接添加。因为默认是最后
+	        	parent.appendChild(newElement );
+	   		} else {
+	        	//如果不是，则插入在目标元素的下一个兄弟节点的前面。也就是目标元素的后面
+	        	parent.insertBefore( newElement, targetElement.nextSibling );
+	   		}
+		},
+		labelText : ""
+	});
+}
+
